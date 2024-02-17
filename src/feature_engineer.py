7a2e627ecb18adc86fbe5ced586c32a11d7b023d
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-17 17:48:40 +0000

    Refactor feature_engineer and train functions

--- src/feature_engineer.py
+++ src/feature_engineer.py
@@ -1,4 +1,2 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
 def feature_engineer(X_train, X_test, y_train, y_test):
-    print("Feature Engineering")
+	print("Feature Engineering")
--- src/train.py
+++ src/train.py
@@ -1,3 +1,2 @@
-
 def train(X_train, X_test, y_train, y_test):
-    print("Train!")
+	print("Train!")
