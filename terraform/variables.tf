ee1c0a13cd286ce2c148bb556935b6ae3f0cbf56
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-17 19:11:21 +0000

    Refactor Terraform configuration for Lambda and S3 modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,59 +1,13 @@
-variable "s3_bucket_id" {
-  description = "The ID of the S3 bucket to which the Lambda function will be subscribed"
-  type        = string
-}
-
-variable "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket to which the Lambda function will be subscribed"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-  default     = "train_test_bucket"
-}
-
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
-  default     = "predict"
-  type        = string
+variable "name" {
+  default     = "example-2024-02"
+  description = "Application name"
 }
 
 variable "environment" {
   description = "The environment the bucket is used in [DEV, STAG, PROD]"
+  default     = "DEV"
   validation {
     condition     = contains(["DEV", "STAG", "PROD"], var.environment)
     error_message = "Environment must be one of DEV, STAG, PROD"
   }
-}
-
-variable "application_name" {
-  description = "Application name"
-}
-
-variable "memory_size" {
-  description = "The amount of memory in MB that Lambda Function can use at runtime"
-  type        = number
-  default     = 256
-}
-
-variable "timeout" {
-  description = "The amount of time in seconds that Lambda Function has to run"
-  type        = number
-  # 10 minutes
-  default = 600
-}
-
-variable "python_version" {
-  description = "The Python version to use"
-  type        = string
-  default     = "python3.12"
-}
-
-variable "ecr_repository" {
-  description = "The URL of the ECR repository"
-  type        = string
-  default     = "lambda-repo"
-
 }
