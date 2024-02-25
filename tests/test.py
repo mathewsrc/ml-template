9e670c033cb3eecff3c992c51e0ac9278ce5a95c
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-25 17:03:33 +0000

    Update test case for API response

--- tests/test.py
+++ tests/test.py
@@ -12,4 +12,4 @@ def test():
 def test_api():
 	response = client.post("/predict", json={"price": 10, "temperature": 2})
 	assert response.status_code == 200
-	assert 20 == 20
+ 	assert response.json() == {"result": 20}
