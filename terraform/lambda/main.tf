31e609f917d3b2097e1ebc1fa4fe228b9a5311c4
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-25 16:03:20 +0000

    Add ECR repository creation and provisioning and remove policy resources

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1,89 +1,23 @@
-# Create an IAM policy document for the lambda
-data "aws_iam_policy_document" "lambda_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["sts:AssumeRole"]
-    resources = ["*"]
-  }
-
-  statement {
-    sid    = "CloudWatchAccess"
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:*:*"]
-  }
-
-  statement {
-    sid = "S3Access"
-
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:ListBucket",
-      "s3:DeleteObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }
-}
-
-# Create an IAM policy for the lambda
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "IAM policy for lambda"
-  policy      = data.aws_iam_policy_document.lambda_policy.json
-}
-
-# Create an IAM role for the lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-# Attach the policy to the lambda role
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
-
 # Get current AWS region
 data "aws_region" "current" {}
 
 # Get current AWS account ID
 data "aws_caller_identity" "current" {}
 
-resource "null_resource" "package_lambda" {
-  triggers = {
-    files = "${filebase64sha256("${path.module}/../../Dockerfile")}"
-    files = "${filebase64sha256("${path.module}/../../api/main.py")}"
-    files = "${filebase64sha256("${path.module}/../../deploy.sh")}"
-  }
-  provisioner "local-exec" {
-    command     = "chmod +x ${path.module}/../../deploy.sh; ${path.module}/../../deploy.sh"
-    interpreter = ["bash", "-c"]
-  }
-}
+data "aws_ecr_authorization_token" "token" {}
+
+
+# resource "null_resource" "package_lambda" {
+#   triggers = {
+#     dockerfilehash = "${filebase64sha256("${path.module}/../../Dockerfile")}"
+#     apihash = "${filebase64sha256("${path.module}/../../api/main.py")}"
+#     deployhash = "${filebase64sha256("${path.module}/../../deploy.sh")}"
+#   }
+#   provisioner "local-exec" {
+#     command     = "chmod +x ${path.module}/../../deploy.sh; ${path.module}/../../deploy.sh"
+#     interpreter = ["bash", "-c"]
+#   }
+# }
 
 data "external" "envs" {
   program = ["bash", "-c", <<-EOSCRIPT
@@ -96,15 +30,40 @@ data "external" "envs" {
   ]
 }
 
+resource "aws_ecr_repository" "repository" {
+  name                 = "lambda-${exeternal.envs.result.project_name}"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+
+  # Ref: https://stackoverflow.com/questions/69907325/terraform-aws-lambda-function-requires-docker-image-in-ecr
+  provisioner "local-exec" {
+    #    This is a 1-time execution to put a dummy image into the ECR repo, so 
+    #    terraform provisioning works on the lambda function. Otherwise there is
+    #    a chicken-egg scenario where the lambda can't be provisioned because no
+    #    image exists in the ECR
+    command     = <<EOF
+      docker login ${data.aws_ecr_authorization_token.token.proxy_endpoint} -u AWS -p ${data.aws_ecr_authorization_token.token.password}
+      docker pull alpine
+      docker tag alpine ${aws_ecr_repository.repository.repository_url}:${data.external.envs.result.sha}
+      docker push ${aws_ecr_repository.repository.repository_url}:${data.external.envs.result.sha}
+      EOF
+  }
+}
+
 # Create a Lambda Function
 resource "aws_lambda_function" "func" {
   depends_on = [
     aws_iam_role_policy_attachment.lambda_policy_attachment,
-    null_resource.package_lambda,
+    #null_resource.package_lambda,
     data.external.envs
   ]
   role          = aws_iam_role.lambda_role.arn
-  image_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${data.external.envs.result.project_name}:${data.external.envs.result.sha}"
+  image_uri     = "${aws_ecr_repository.image_storage.repository_url}:${data.external.envs.result.sha}"
   function_name = var.lambda_function_name
   description   = "Model serving function for ${var.application_name}"
   memory_size   = var.memory_size
