0ec777c81640af1ffb6bada6ebf40eb39146cddb
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-25 16:25:32 +0000

    Update file paths and names in dvc_pipeline.sh

--- scripts/dvc_pipeline.sh
+++ scripts/dvc_pipeline.sh
@@ -4,13 +4,13 @@
 
 poetry run dvc stage add \
     -n preprocess \ 
-    -d preprocess.py -d utils.py -d raw_dataset/raw_data.csv \ 
-    -o processed_dataset/processed_data.csv \ 
+    -d preprocess.py -d utils.py -d raw_dataset/weather.csv \ 
+    -o processed_dataset/weather.csv \ 
     python src/preprocess.py
 
 poetry run dvc stage add \
     -n train \
-    -d train.py -d metrics_and_plots.py -d utils.py -d processed_dataset/processed_data.csv \
+    -d train.py -d metrics_and_plots.py -d utils.py -d processed_dataset/weather.csv \
     -o model.pkl -o confusion_matrix.png -o metrics.json \
     python src/train.py
 
