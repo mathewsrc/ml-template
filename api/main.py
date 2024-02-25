fe762a3cc98ff7e9827564f4e5cf6592877a679c
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-25 16:02:48 +0000

    Add logging and AWS S3 integration

--- api/main.py
+++ api/main.py
@@ -4,38 +4,38 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import mangum
 import uvicorn
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+AWS_REGION = os.getenv("REGION")
 
 app = FastAPI()
-
 
 class Body(BaseModel):
 	text: str
 	temperature: float = 0.5
 
-
 load_dotenv()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-	return HTMLResponse(
-		"""
-    <h1>Welcome to</p>
-    """
-	)
-
 
 @app.post("/predict")
 async def question(body: Body):
 	try:
+		bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    	object_key = event["Records"][0]["s3"]["object"]["key"]
 		predict = 10
+		logger.info(f"Predicted value: {predict}")
 	except Exception as e:
 		raise HTTPException(status_code=500, detail=str(e))
 	return JSONResponse({"result": predict})
 
-
 handler = mangum.Mangum(app)
+
+def lambda_handler(event, context):
+    """AWS Lambda function handler."""
+    return {"statusCode": 200, "body": json.dumps("Successful!")}
 
 
 if __name__ == "__main__":
