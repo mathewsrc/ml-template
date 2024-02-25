b1ddd3a4f71ee5960a6f47634a9e37b3b396721e
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-25 16:04:43 +0000

    Add test cases for root and question endpoints

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,20 @@
+from fastapi.testclient import TestClient
+
+from api import main
+
+client = TestClient(app)
+
 def test():
 	assert (1, 2, 3) == (1, 2, 3)
 	assert "Hello" == "Hello"
+
+def test_root_endpoint():
+	response = client.get("/")
+	assert response.status_code == 200
+	assert "<h1>Welcome to our Question/Answering application" in response.text
+
+
+def test_question_endpoint():
+	response = client.post("/ask", json={"text": "What is AWS?", "temperature": 0.5})
+	assert response.status_code == 200
+	assert "answer" in response.json()
