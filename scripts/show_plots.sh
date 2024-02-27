5b2a147c5efb1e19ca962ae04a93659792f2fcb0
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-27 15:48:47 +0000

    Add dvc plots diff command to show_plots.sh script

--- scripts/show_plots.sh
+++ scripts/show_plots.sh
@@ -5,9 +5,11 @@
 for plot in plots/*.png; do
     echo "Displaying plot: $plot"
     poetry run dvc plots show $plot
+    poetry run dvc plots diff --target $plot main
 done
 
 for metric in metrics/*.csv; do
     echo "Displaying metric: $metrics"
     poetry run dvc plots show $metric
+    poetry run dvc plots diff --target $metric main
 done
