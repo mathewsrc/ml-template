dcf8c7dfe68f93b24a0626077054e87c2f4514b3
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-27 17:35:06 +0000

    Add data loading and hyperparameter loading functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import shutil
 from pathlib import Path
+import json
+import polars as pl
 
 DATASET_TYPES = ["test", "train"]
 DROP_COLNAMES = ["A_id"]
@@ -15,3 +17,15 @@ def delete_and_recreate_dir(path):
         pass
     finally:
         Path(path).mkdir(parents=True, exist_ok=True)
+
+
+def load_data(file_path):
+    data = pl.read_csv(file_path).to_pandas()
+    X = data.drop(TARGET_COLUMN, axis=1)
+    y = data[TARGET_COLUMN]
+    return X, y
+
+def load_hyperparameters(hyperparameter_file):
+    with open(hyperparameter_file, "r") as json_file:
+        hyperparameters = json.load(json_file)
+    return hyperparameters
