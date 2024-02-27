b240484d468befd49fbbffa046c2db9dda422feb
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-27 20:09:05 +0000

    Refactor code in utils.py

--- src/utils.py
+++ src/utils.py
@@ -11,21 +11,22 @@ PROCESSED_DATASET = "processed_dataset/apple_quality.csv"
 
 
 def delete_and_recreate_dir(path):
-    try:
-        shutil.rmtree(path)
-    except:
-        pass
-    finally:
-        Path(path).mkdir(parents=True, exist_ok=True)
+	try:
+		shutil.rmtree(path)
+	except:
+		pass
+	finally:
+		Path(path).mkdir(parents=True, exist_ok=True)
 
 
 def load_data(file_path):
-    data = pl.read_csv(file_path).to_pandas()
-    X = data.drop(TARGET_COLUMN, axis=1)
-    y = data[TARGET_COLUMN]
-    return X, y
+	data = pl.read_csv(file_path).to_pandas()
+	X = data.drop(TARGET_COLUMN, axis=1)
+	y = data[TARGET_COLUMN]
+	return X, y
+
 
 def load_hyperparameters(hyperparameter_file):
-    with open(hyperparameter_file, "r") as json_file:
-        hyperparameters = json.load(json_file)
-    return hyperparameters
+	with open(hyperparameter_file, "r") as json_file:
+		hyperparameters = json.load(json_file)
+	return hyperparameters
