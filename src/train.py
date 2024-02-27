33288ccd311d3abceae3f2f10c32f2d5c39dae71
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-27 20:08:59 +0000

    Refactor train_model and evaluate_model functions

--- src/train.py
+++ src/train.py
@@ -15,61 +15,65 @@ rf_max_depth = params["train"]["max_depth"]
 rf_n_estimators = params["train"]["n_estimators"]
 random_state = params["random_state"]
 
-def train_model(X_train, y_train,
-                hyperparameters={"max_depth": rf_max_depth, 
-                                 "n_estimators": rf_n_estimators, 
-                                 "random_state": random_state}):
-    model = RandomForestClassifier(**hyperparameters)
-    
-    model.fit(X_train, y_train)
-    
-    # Save model in models/
-    model_path = Path("models/model.pkl")
-    with open(model_path, "wb") as file:
-        pickle.dump(model, file)
-        
-    return model
+
+def train_model(
+	X_train,
+	y_train,
+	hyperparameters={
+		"max_depth": rf_max_depth,
+		"n_estimators": rf_n_estimators,
+		"random_state": random_state,
+	},
+):
+	model = RandomForestClassifier(**hyperparameters)
+
+	model.fit(X_train, y_train)
+
+	# Save model in models/
+	model_path = Path("models/model.pkl")
+	with open(model_path, "wb") as file:
+		pickle.dump(model, file)
+
+	return model
 
 
 def evaluate_model(model, X_test, y_test, float_precision=4):
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    y_proba = model.predict_proba(X_test)[:, 1]
-    roc_auc = roc_auc_score(y_test, y_proba)
-    metrics = {
-        "accuracy": accuracy,
-        "precision": precision,
-        "recall": recall,
-        "f1_score": f1,
-        "roc_auc": roc_auc,
-    }
-    
-    save_predictions(y_test, y_pred)
+	y_pred = model.predict(X_test)
+	accuracy = accuracy_score(y_test, y_pred)
+	precision = precision_score(y_test, y_pred)
+	recall = recall_score(y_test, y_pred)
+	f1 = f1_score(y_test, y_pred)
+	y_proba = model.predict_proba(X_test)[:, 1]
+	roc_auc = roc_auc_score(y_test, y_proba)
+	metrics = {
+		"accuracy": accuracy,
+		"precision": precision,
+		"recall": recall,
+		"f1_score": f1,
+		"roc_auc": roc_auc,
+	}
 
-    return json.loads(
-        json.dumps(metrics), parse_float=lambda x: round(float(x), float_precision)
-    )
+	save_predictions(y_test, y_pred)
+
+	return json.loads(json.dumps(metrics), parse_float=lambda x: round(float(x), float_precision))
 
 
 def main():
-    X, y = load_data(PROCESSED_DATASET)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1993)
+	X, y = load_data(PROCESSED_DATASET)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1993)
 
-    hyperparameters = load_hyperparameters("hyperparameters.json")
-    model = train_model(X_train, y_train, hyperparameters=hyperparameters)
-    metrics = evaluate_model(model, X_test, y_test)
+	hyperparameters = load_hyperparameters("hyperparameters.json")
+	model = train_model(X_train, y_train, hyperparameters=hyperparameters)
+	metrics = evaluate_model(model, X_test, y_test)
 
-    print("====================Test Set Metrics==================")
-    print(json.dumps(metrics, indent=2))
-    print("======================================================")
+	print("====================Test Set Metrics==================")
+	print(json.dumps(metrics, indent=2))
+	print("======================================================")
 
-    save_metrics(metrics)
-    plot_confusion_matrix(model, X_test, y_test)
-    save_roc_auc(model, X_test, y_test)
+	save_metrics(metrics)
+	plot_confusion_matrix(model, X_test, y_test)
+	save_roc_auc(model, X_test, y_test)
+
 
 if __name__ == "__main__":
-    main()
-
+	main()
