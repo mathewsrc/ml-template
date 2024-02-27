d02c5306adad571b65a00a102d7ac725e6781c1a
Matheus Ribeiro <94936606+mathewsrc@users.noreply.github.com>
2024-02-27 14:41:05 +0000

    Update confusion matrix plot and add prediction saving

--- src/metrics_and_plots.py
+++ src/metrics_and_plots.py
@@ -1,14 +1,18 @@
 import json
-
+import polars as pl
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 
 
 def plot_confusion_matrix(model, X_test, y_test):
     _ = ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, cmap=plt.cm.Blues)
-    plt.savefig("reports/confusion_matrix.png")
-
+    plt.savefig("plots/confusion_matrix.png")
 
 def save_metrics(metrics):
     with open("metrics/metrics.json", "w") as fp:
         json.dump(metrics, fp)
+        
+def save_predictions(y_true, y_pred):
+    # Save predictions as csv in metrics/
+    predictions = pl.DataFrame({"y_true": y_true, "y_pred": y_pred})
+    predictions.write_csv("metrics/predictions.csv")
